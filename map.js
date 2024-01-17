c16f2da9177f5acbf82ce15506bb50f6e2d45714
Marceau <marceau.combet@gmail.com>
2024-01-17 15:20:19 +0100

    modified distance for coordinates

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-import * as fs from 'fs';
+//import * as fs from 'fs';
 
 const R = 6371e3; // Earth radius metres
 
@@ -35,13 +35,24 @@ function readSitesFromJSON(jsonName) {
   });
 }
 
+function distance(coord1, coord2) {
+  const r = 6371e3; // meter
+  const p = Math.PI / 180;
+
+  const a = 0.5 - Math.cos((coord2.lat - coord1.lat) * p) / 2
+                + Math.cos(coord1.lat * p) * Math.cos(coord2.lat * p) *
+                  (1 - Math.cos((coord2.lon - coord1.lon) * p)) / 2;
+
+  return 2 * r * Math.asin(Math.sqrt(a));
+}
 
 
-function distance(site1, site2) {
-    var φ1 = site1.Coordinates.lat * Math.PI/180; // φ, λ in radians
-    var φ2 = site2.Coordinates.lat * Math.PI/180;
-    var Δφ = (site2.Coordinates.lat - site1.Coordinates.lat) * Math.PI/180;
-    var Δλ = (site2.Coordinates.lon - site1.Coordinates.lon) * Math.PI/180;
+/*
+function distance(coord1, coord2) {
+    var φ1 = coord.lat * Math.PI/180; // φ, λ in radians
+    var φ2 = coord2.lat * Math.PI/180;
+    var Δφ = (coord2.lat - coord1.lat) * Math.PI/180;
+    var Δλ = (coord2.lon - coord1.lon) * Math.PI/180;
     
     var haversine = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
             Math.cos(φ1) * Math.cos(φ2) *
@@ -49,5 +60,6 @@ function distance(site1, site2) {
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
     const d = R * c; // in metres
-}
+    return d;
+}*/
 
