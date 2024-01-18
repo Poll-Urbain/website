b74549a6adbe19def09228cb4e5c7e1ecf073da1
Theault-dev <tiroroti@msn.com>
2024-01-18 11:49:49 +0100

    create pin.js

---
+++ js/pin.js
@@ -0,0 +1,15 @@
+function defaultPin() {
+    // get name of the id Element
+    var pinElementId;
+    // get name of the building
+    var pinBuildingName
+    // get the image of the pin location
+    var pinImgBefore;
+    var pinLocation = document.getElementById(pinElementId);
+    var defaultPin =
+        "<div class='popup-content'>" +
+        '<b>' + pinName + '</b><br>' +
+        '<img src=' + pinImgBefore + ' Image" style="width:100%; height:auto;">' +
+        '<br><button onclick="onButtonClick()">Voter pour</button>';
+
+}
