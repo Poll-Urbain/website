1abfe982e1ac893cb5f20e8694d405ebcb0bb478
ArthurMaurice <arthur17maurice@gmail.com>
2024-01-18 17:21:09 +0100

    Add zone pin in order to rank votes

---
+++ js/rank.js
@@ -0,0 +1,63 @@
+imageContainer = document.getElementById('image-container');
+const images = ['campus1.jpg', 'campus2.jpg', 'campus4.jpg'];
+let draggedItem = null;
+
+// Dynamically load images into the container
+images.forEach(image => {
+    const voteElement = document.createElement('div');
+    const imgElement = document.createElement('img');
+    imgElement.src = "images/"+image;
+    imgElement.style = "width: 100%; height: auto;";
+    const caracteristics = document.createElement('div');
+    const caracteristic1 = document.createElement('div');
+    const caracteristic2 = document.createElement('div');
+    const caracteristic3 = document.createElement('div');
+    voteElement.appendChild(imgElement);
+    caracteristic1.innerHTML = "Chaleur   <select id=\"dropdown\">" +
+        "<option value=\"1\">Rank 1</option>" +
+        "<option value=\"2\">Rank 2</option>" +
+        "<option value=\"3\">Rank 3</option>" +
+        "</select>";
+    caracteristic2.innerHTML = "Inondation   <select id=\"dropdown\">" +
+        "<option value=\"1\">Rank 1</option>" +
+        "<option value=\"2\">Rank 2</option>" +
+        "<option value=\"3\">Rank 3</option>" +
+        "</select>";
+    caracteristic3.innerHTML = "Air   <select id=\"dropdown\">" +
+        "<option value=\"1\">Rank 1</option>" +
+        "<option value=\"2\">Rank 2</option>" +
+        "<option value=\"3\">Rank 3</option>" +
+        "</select>";
+    caracteristics.appendChild(caracteristic1);
+    caracteristics.appendChild(caracteristic2);
+    caracteristics.appendChild(caracteristic3);
+    voteElement.appendChild(caracteristics);
+    imageContainer.appendChild(voteElement);
+});
+
+// Enable drag-and-drop functionality
+
+imageContainer.addEventListener('dragstart', (e) => {
+    draggedItem = e.target;
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', draggedItem);
+});
+
+imageContainer.addEventListener('dragover', (e) => {
+    e.preventDefault();
+});
+
+imageContainer.addEventListener('drop', (e) => {
+    e.preventDefault();
+    if (e.target.tagName === 'IMG') {
+        const droppedItem = e.target;
+        const temp = document.createElement('div');
+
+        // Swap the positions of the dragged and dropped items
+        draggedItem.parentNode.insertBefore(temp, draggedItem);
+        droppedItem.parentNode.insertBefore(draggedItem, droppedItem);
+        temp.parentNode.insertBefore(droppedItem, temp);
+
+        temp.parentNode.removeChild(temp);
+    }
+});
